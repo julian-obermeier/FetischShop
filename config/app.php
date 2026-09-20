5c1f79f069ce07de683a2c31ff9ab4cd36801e4a
julian-obermeier <jeyohtv@gmail.com>
2026-09-20 14:03:40 +0200

    build: add config/app.php

---
+++ config/app.php
@@ -0,0 +1,12 @@
+<?php
+return [
+'name'=>'FetischShop',
+'timezone'=>'Europe/Berlin',
+'currency'=>'EUR',
+'locale'=>'de_DE',
+'base_url'=>'',
+'private_storage'=>dirname(__DIR__).'/storage/private',
+'log_path'=>dirname(__DIR__).'/storage/logs/app.log',
+'mail_from'=>'noreply@localhost',
+'mail_from_name'=>'FetischShop',
+];
